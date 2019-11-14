44639aaed8ef320a386112d771cf4828ca52c710
Anika Nawar <nawarani47@gmail.com>
2019-11-14 15:09:36 -0500

    more changes

--- app/main.py
+++ app/main.py
@@ -144,4 +144,4 @@ def predict():
         return render_template('results.html', prediction=pred_df, test = test)
 
 if __name__ == '__main__':
-    app.run(debug = True, port=80)
+    app.run(host='0.0.0.0', port=80)
